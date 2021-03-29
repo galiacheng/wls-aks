0c95a675e964305072b1bc4d1f95c9d644e38861
haixia <haixia.cheng@microsoft.com>
2021-03-29 17:11:32 +0800

    demo2

---
+++ auto-scaling/webhook/scripts/scaleDownAction.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo scale up action >> scaleup.log
+
+MASTER=https://haicheakscnipublic-dns-069d2dd0.hcp.southeastasia.azmk8s.io:443
+
+echo Kubernetes master is $MASTER
+
+source /var/scripts/scalingAction.sh \
+  --action=scaleDown \
+  --domain_uid=sample-domain1 \
+  --cluster_name=cluster-1 \
+  --kubernetes_master=${MASTER} \
+  --wls_domain_namespace=sample-domain1-ns \
+  --operator_service_name=internal-weblogic-operator-svc \
+  --operator_service_account=sample-weblogic-operator-sa \
+  --operator_namespace=sample-weblogic-operator-ns \
+  --scaling_size=1
