9c4d8b53a12e5898f684f67a24cfbebe939aa51f
galiacheng <haixia.cheng@mircosoft.com>
2021-04-25 10:35:09 +0800

    check tooling

--- arm-scripts/scripts/test.sh
+++ arm-scripts/scripts/test.sh
@@ -1,1 +1,15 @@
-echo "test"
+echo "Script starts"
+echo "docker Version"
+docker --version
+echo "kubectl version"
+kubectl version
+echo "helm version"
+helm versiob
+echo "java version"
+java -version
+echo "az cli version"
+az --version
+echo "git version"
+git --version
+echo "unzip version"
+unzip --version
