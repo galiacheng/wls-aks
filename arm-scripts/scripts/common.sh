7a5980551739b9c8c8dd63ca67f68e633022cc07
haixia.cheng@microsoft.com <APB9faTH>
2021-07-19 11:20:18 +0800

    update scripts

--- arm-scripts/scripts/common.sh
+++ arm-scripts/scripts/common.sh
@@ -1,3 +1,5 @@
+export constFalse="false"
+export constTrue="true"
 export curlMaxTime=120 # seconds
 export optUninstallMaxTry=5 # Max try number to wait for the operator uninstalled
 export optUninstallInterval=10
